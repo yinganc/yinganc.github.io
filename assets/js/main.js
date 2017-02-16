8e6dd2cbcfa6ec0a7a6c752c6deb66fc4070a85a
yinganc <yinganc@yingancde-MacBook-Air.local>
2017-02-15 22:27:16 -0500

    add visual design section

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,8 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+
 
 (function($) {
 
